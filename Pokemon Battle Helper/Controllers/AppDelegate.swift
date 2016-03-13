b719ddbf7d9d8d746a987e9f610de0e394dfa598
Yeung Yiu Hung <hkclex@gmail.com>
2016-03-13 19:57:32 +0800

    Initial Commit

---
+++ Pokemon Battle Helper/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Pokemon Battle Helper
+//
+//  Created by Yeung Yiu Hung on 3/13/16
+//  Copyright (c) 2016 . All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
